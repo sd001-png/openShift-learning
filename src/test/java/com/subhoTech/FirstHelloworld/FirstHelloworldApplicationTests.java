23574ee025e6086bea92c7a5fccf14ea009274dd
Subhadip <subha130394@gmail.com>
2021-04-14 11:20:12 -0700

    my first commit

---
+++ src/test/java/com/subhoTech/FirstHelloworld/FirstHelloworldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.subhoTech.FirstHelloworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstHelloworldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
